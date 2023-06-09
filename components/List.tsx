54ec5ed50e775c2129f7b8341a15db1485ff1d5b
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-06-09 14:31:54 -0300

    feat: Implemented List page

--- components/List.tsx
+++ components/List.tsx
@@ -1,45 +1,47 @@
+import Link from "next/link";
 import { BsPatchCheckFill } from "react-icons/bs";
 
 const List = () => {
   const items = [
     {
       id: 1,
-      text: "Altamente efectivo",
+      text: "Producto en tendencia",
     },
-    { id: 2, text: "Concentrado, con una duración de 6 a 7 meses" },
+    { id: 2, text: "Su costo es accesible" },
     {
       id: 3,
-      text: "Blanquea sin dañar el esmalte",
+      text: "La mejor calidad",
     },
     {
       id: 4,
-      text: "Mayor efectividad, requiere menos fluoruro y es más seguro",
+      text: "Promueve el bienestar",
     },
     {
       id: 5,
-      text: "Producto exclusivo",
-    },
-    { id: 6, text: "Producto extremadamente económico" },
-    {
-      id: 7,
-      text: "Garantía de Satisfacción",
+      text: "Hecho con igredientes naturales",
     },
     {
-      id: 8,
-      text: "Reduce 6 envases por unidad comprada en comparación con los dentífricos más comunes",
+      id: 6,
+      text: "Promueve una vida saludable",
     },
   ];
+
+  const animationVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+  };
+
   return (
     <section className="w-full mx-auto py-10 mdl:py-24 flex flex-col items-center gap-4 lgl:gap-8 mdl:px-10 xl:px-4">
       <div className="w-10/12 flex flex-col items-center">
-        <div className="block max-w-container p-6 bg-gradient-to-r from-black to-gray-900 transition-300  rounded-lg border-1 shadow-md border-btnColor1 drop-shadow-sm shadow-btnColor1">
-          <h2 className="mb-2 xs:text-xl sm:text-xl md:text-xl lg:text-4xl font-bold tracking-tight bg-gradient-to-r from-btnColor2 to-btnColor1 text-transparent bg-clip-text text-center">
-            TODO LO QUE RECIBES AL <br /> ADQUIRIR NUESTRA PASTA DENTAL
+        <div className="block max-w-container p-6 bg-gradient-to-r from-colorBaseOne to-colorBaseTwo transition-300  rounded-lg border-1 shadow-md border-colorBaseTwo drop-shadow-sm shadow-colorBaseTwo mb-2">
+          <h2 className="mb-2 xs:text-xl sm:text-xl md:text-xl lg:text-4xl font-bold tracking-tight  text-textLight text-center uppercase">
+            TODO LO QUE RECIBES AL <br /> ADQUIRIR NUESTRO OMEGA 3
           </h2>
           <ul className="flex flex-col items-center">
             {items.map((item) => (
               <li key={item.id} className="flex items-center py-2">
-                <BsPatchCheckFill className="text-btnColor1 mr-2" />
+                <BsPatchCheckFill className="text-textLight mr-2" />
                 <p className="font-bodyFont xs:text-xs sm:text-sm md:text-base lg:text-1xl">
                   {item.text}
                 </p>
@@ -47,20 +49,19 @@ const List = () => {
             ))}
           </ul>
           <div className="flex flex-col items-center">
-            <p className="xs:text-xl sm:text-xl md:text-2xl lg:text-4xl font-bodyFont tracking-tight text-center">
+            <p className="xs:text-base sm:text-base md:text-2xl lg:text-4xl font-bodyFont tracking-tight text-center">
               1x de{" "}
-              <span className="xs:text-5xl sm:text-5xl md:text-6xl lg:text-8xl font-bodyFont font-semibold text-textPriceColor animate-pulse">
-                $ 1.433,88
+              <span className="xs:text-3xl sm:text-3xl md:text-6xl lg:text-8xl font-bodyFont font-semibold text-textPriceColor animate-pulse">
+                $ 3.422,00
               </span>
             </p>
-            {/* <p className="xs:text-xl sm:text-xl md:text-2xl lg:text-4xl font-bodyFont uppercase pt-10">
-              O 997,00 1.433,88
-            </p> */}
           </div>
         </div>
-        <button className="w-2/5 h-14 text-sm font-titleFont rounded-md text-textGreen tracking-wide bg-gradient-to-r from-btnColor1 to-btnColor2 hover:from-btnColor2 hover:to-btnColor1 font-bold py-2 px-4 animate-bounce border shadow-md shadow-btnColor1 xs:text-sm xs:h-20 xs:mt-5 uppercase">
-          Voy a comprar mi pasta ahora!
-        </button>
+        <Link href="https://wa.me/5524981579339" target="_blank">
+          <button className="w-full my-10 text-sm rounded-md text-textGreen tracking-wide bg-gradient-to-r from-colorBaseOne to-colorBaseTwo hover:from-colorBaseTwo hover:to-colorBaseOne font-bold py-2 xs:px-5 sm:px-5 md:px-8 lg:px-40 animate-bounce border shadow-md shadow-colorBaseTwo xs:h-20 uppercase font-titleSecondFont">
+            Quiero mi omega ahora!
+          </button>
+        </Link>
       </div>
     </section>
   );
