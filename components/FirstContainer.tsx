5767ff7ecc6aba431c20fddde3c726fca787fe1c
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-06-09 14:29:43 -0300

    feat: Implemented FirstContainer page

--- components/FirstContainer.tsx
+++ components/FirstContainer.tsx
@@ -2,22 +2,24 @@ import { motion } from "framer-motion";
 
 const FirstContainer = () => {
   return (
-    <section className="w-full mx-auto py-10 mdl:py-24 flex flex-col items-center gap-4 lgl:gap-8 mdl:px-10 xl:px-4 bg-gradient-to-r from-btnColor1 to-btnColor2 transition-300 shadow-2xl">
+    <section className="w-full mx-auto py-10 mdl:py-24 flex flex-col items-center gap-4 lgl:gap-8 mdl:px-10 xl:px-4 bg-gradient-to-r from-colorBaseOne to-colorBaseTwo transition-300 shadow-2xl">
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 1, delay: 0.7 }}
+        transition={{ duration: 1, delay: 0.6 }}
         className="w-10/12 my-10"
       >
-        <h2 className="font-titleFont font-bold tracking-wide text-center uppercase xs:text-lg sm:text-lg md:text-4xl lg:text-4xl xs:pt-5 sm:pt-5">
-          Ayuda a combatir las caries y también a eliminar la placa
+        <h2 className="font-titleFont font-normal tracking-wide text-center xs:text-lg sm:text-lg md:text-4xl lg:text-4xl xs:pt-5 sm:pt-5">
+          <span className="font-bold">Contiene</span> una mezcla única de peces
+          de agua fría:{" "}
+          <span className="font-bold"> Sardinas, Caballa y Anchoas</span>.
         </h2>
-        <h2 className="text-4xl font-titleFont tracking-wide text-center mt-4 xs:text-lg sm:text-lg md:text-4xl lg:text-4xl xs:pb-5 sm:pb-5">
-          Promueve la remineralización, ayuda a fortalecer y proteger el
-          esmalte. <br />{" "}
+        <h2 className="text-4xl font-titleSecondFont tracking-wide text-center mt-4 xs:text-lg sm:text-lg md:text-4xl lg:text-4xl xs:pb-5 sm:pb-5">
+          Los ácidos grasos omega 3 colaboran con la salud del sistema
+          cardiovascular. <br />{" "}
           <span className="bg-black">
-            Además limpia y refresca el aliento de forma segura cada vez que te
-            cepillas los dientes.
+            Contiene vitamina E, un potente antioxidante que ayuda mantener la
+            calidad de los ácidos grasos omega 3.
           </span>
         </h2>
       </motion.div>
