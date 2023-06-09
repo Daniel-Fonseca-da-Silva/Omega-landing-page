e25ddf7a833b04579e1a61680bfa40ed81b8fb3d
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-06-09 14:30:42 -0300

    feat: Implemented ThirdContainer page

--- components/ThirdContainer.tsx
+++ components/ThirdContainer.tsx
@@ -1,76 +1,45 @@
+import { chicaConSonrisa } from "@/public/assets";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
-const ThirdContainer = () => {
+const ThirdContainerr = () => {
   return (
-    <section className="w-full mx-auto py-10 mdl:py-24 flex flex-col items-center gap-4 lgl:gap-8 mdl:px-10 xl:px-4">
-      <div className="flex flex-col md:flex-row justify-center my-10">
-        <div className="md:w-3/5">
-          <h2 className="lg:text-4xl xs:text-base sm:text-lg md:text-lg lg:px-10 font-titleFont font-bold tracking-wide uppercase xs:text-center sm:text-center">
-            O QUE É O{" "}
-            <span className="bg-gradient-to-r from-btnColor1 to-btnColor2 text-transparent bg-clip-text text-center">
-              MÉTODO CVL?
-            </span>
-          </h2>
-          <p className="lg:mb-2 xs:text-center lg:px-10 lg:text-2xl mt-5 font-semibold font-bodyFont tracking-tight leading-10 xs:text-base sm:text-base md:text-lg xs:px-10 sm:px-10 sm:pb-5 text-textDark">
-            O Método CVL foi desenvolvido por Enrico Ganchiel,{" "}
-            <span className="bg-gradient-to-r from-btnColor1 to-btnColor2 text-transparent bg-clip-text">
-              após mais de uma década com centenas de arrematações e milhões de
-              reais investidos no mercado de moedas. Esse passo a passo
-              <br /> <br />
-              <span className="bg-gradient-to-r from-btnColor1 to-btnColor2 text-transparent bg-clip-text">
-                super detalhado e simples de entender, pode te trazer uma alta
-                lucratividade já nas próximas semanas.
-              </span>
-            </span>
-          </p>
+    <section className="w-full mx-auto mdl:py-24 flex flex-col items-center gap-4 lgl:gap-8 mdl:px-10 xl:px-4">
+      <div className="w-10/12 my-10">
+        <div className="mx-auto">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            <div className="block p-2 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+              <Image
+                className="w-full rounded-lg"
+                src={chicaConSonrisa}
+                alt="Chica Argentina con una sonrisa guapa"
+              />
+            </div>
+          </motion.div>
         </div>
-        <div className="md:w-2/5">
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1, delay: 0.7 }}
-            className="md:px-20 md:py-10 xs:py-10 sm:py-10 xs:mr-4 xs:mb-5 bg-btnColor1 border border-btnColor1 rounded-lg shadow bg-gradient-to-r from-cardColor to-bodyColor hover:from-bodyColor hover:to-cardColor"
-          >
-            <p className="md:mb-2 lg:text-2xl xs:text-sm sm:text-base font-semibold font-bodyFont -tracking-wide text-center">
-              Não sabe nada sobre o mercado de cripto:
-            </p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1, delay: 0.7 }}
-            className="md:px-20 md:py-10 xs:py-10 sm:py-10 xs:mr-4 xs:mb-5 bg-btnColor1 border border-btnColor1 rounded-lg shadow bg-gradient-to-r from-cardColor to-bodyColor hover:from-bodyColor hover:to-cardColor"
-          >
-            <p className="md:mb-2 lg:text-2xl xs:text-sm sm:text-base font-semibold font-bodyFont -tracking-wide text-center">
-              Está buscando uma forma segura e rentável de investir o seu
-              dinheiro:
-            </p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1, delay: 0.7 }}
-            className="md:px-20 md:py-10 xs:py-10 sm:py-10 xs:mr-4 xs:mb-5 bg-btnColor1 border border-btnColor1 rounded-lg shadow bg-gradient-to-r from-cardColor to-bodyColor hover:from-bodyColor hover:to-cardColor"
-          >
-            <p className="md:mb-2 lg:text-2xl xs:text-sm sm:text-base font-semibold font-bodyFont -tracking-wide text-center">
-              Não tem muito dinheiro, mas gostaria de investir:
-            </p>
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1, delay: 0.7 }}
-            className="md:px-20 md:py-10 xs:py-10 sm:py-10 xs:mr-4 xs:mb-5 bg-btnColor1 border border-btnColor1 rounded-lg shadow bg-gradient-to-r from-cardColor to-bodyColor hover:from-bodyColor hover:to-cardColor"
-          >
-            <p className="md:mb-2 lg:text-2xl xs:text-sm sm:text-base font-semibold font-bodyFont -tracking-wide text-center">
-              Quer investir em poupança de criptomoedas:
-            </p>
-          </motion.div>
+        <div className="flex flex-col md:flex-row justify-center pt-2">
+          <div className="xs:pl-0 sm:pl-0 md:pl-10 lg:pl-0 xs:py-10 sm:py-10 md:py-0 sm:text-center ">
+            <div className="border-colorBaseOne shadow-md shadow-colorBaseOne PX bg-gray-100 hover:shadow-colorBaseTwo py-5 px-5">
+              <p className="lg:text-4xl xs:text-base sm:text-xl font-bodyFont tracking-wide leading-normal text-textDark">
+                <span className="bg-gradient-to-r from-colorBaseOne to-colorBaseTwo text-transparent bg-clip-text font-bold uppercase">
+                  Evitar la oxidación:
+                </span>{" "}
+                Los ácidos grasos, Nutrilite cuenta con un proceso de alta
+                tecnología y calidad, con el cual los nutrientes extraídos son
+                rápidamente encapsulados al vacío, agregando vitamina E para
+                preservarlos.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="w-1/5 border border-t border-gray-700"></div>
+      <div className="w-1/5 border border-t border-colorBaseTwo"></div>
     </section>
   );
 };
 
-export default ThirdContainer;
+export default ThirdContainerr;
