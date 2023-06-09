e1fbcdf0cfb963bb7a955dd81a2f196c62db48fa
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-06-09 14:33:45 -0300

    feat: Implemented videoPlayer class

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -1,8 +1,8 @@
 const VideoPlayer = () => {
   return (
     <video className="w-full" controls>
-      <source src="/PastaDentalGlister.mp4" type="video/mp4" />
-      Seu navegador não suporta a tag de vídeo.
+      <source src="/omega-3-nutrilite.mp4" type="video/mp4" />
+      Tu navegador no es compatible con la etiqueta de vídeo.
     </video>
   );
 };
