29463adb63230d1cb1530c55a33b6d8f0d553f5d
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-06-09 14:33:27 -0300

    feat: Implemented Terms of user page

---
+++ pages/terms.tsx
@@ -0,0 +1,126 @@
+import React from "react";
+
+const Terms = () => {
+  return (
+    <section className="w-full mx-auto mdl:py-24 flex flex-col items-center gap-4 lgl:gap-8 mdl:px-10 xl:px-4 text-2xl bg-bodyColor">
+      <h1 className="font-titleFont xs:text-lg xs:text-center sm:text-lg xs:pt-10 md:pt-0 md:text-lg lg:text-5xl font-semibold uppercase text-textDark text-center">
+        Política de Privacidad de Nutrilite Omega 3
+      </h1>
+      <p className="xs:text-xs sm:text-sm xs:text-center md:text-base lg:text-lg font-bodyFont tracking-wide leading-normal text-textDark">
+        Fecha de vigencia: [09-06-2023]
+      </p>
+      <p className="xs:text-xs sm:text-sm xs:text-center md:text-base lg:text-lg font-bodyFont tracking-wide leading-normal text-textDark">
+        Agradecemos su interés en los productos de Nutrilite Omega 3. En
+        Nutrilite, una marca de la empresa Amway, nos comprometemos a proteger
+        su privacidad y mantener la confidencialidad de la información personal
+        que usted nos proporciona. Esta política de privacidad tiene como
+        objetivo informarle cómo recopilamos, utilizamos y protegemos su
+        información personal cuando utiliza nuestros productos o servicios. Al
+        utilizar los productos de Nutrilite Omega 3, usted acepta las prácticas
+        descritas en esta política de privacidad.
+      </p>{" "}
+      <h2 className="font-titleFont xs:text-base xs:text-center sm:text-base xs:pt-10 md:pt-0 md:text-lg lg:text-5xl font-semibold uppercase text-textDark text-center">
+        Recopilación de información personal
+      </h2>
+      <p className="xs:text-xs sm:text-sm xs:text-center md:text-base lg:text-lg font-bodyFont tracking-wide leading-normal text-textDark">
+        Recopilamos información personal que usted nos proporciona de forma
+        voluntaria cuando utiliza nuestros productos o servicios. Esto puede
+        incluir, entre otros, su nombre, dirección de correo electrónico, número
+        de teléfono y dirección de envío. Además, podemos recopilar información
+        sobre su salud y bienestar con el fin de proporcionarle recomendaciones
+        y sugerencias personalizadas relacionadas con el uso de nuestros
+        productos de Omega 3.
+      </p>{" "}
+      <h3 className="font-titleFont xs:text-base xs:text-center sm:text-base xs:pt-10 md:pt-0 md:text-lg lg:text-5xl font-semibold uppercase text-textDark text-center">
+        Uso de la información personal
+      </h3>
+      <p className="xs:text-xs sm:text-sm xs:text-center md:text-base lg:text-lg font-bodyFont tracking-wide leading-normal text-textDark">
+        Utilizamos la información personal recopilada para los siguientes fines:
+      </p>{" "}
+      <ul>
+        <li className="xs:text-xs sm:text-sm xs:text-center md:text-base lg:text-lg font-bodyFont tracking-wide leading-normal text-textDark">
+          Procesamiento de pedidos: Utilizamos su información personal para
+          procesar y enviar sus pedidos de productos de Nutrilite Omega 3.
+          Comunicación y servicio al cliente: Utilizamos su información personal
+          para responder a sus consultas, preguntas y solicitudes de servicio al
+          cliente.
+        </li>
+        <li className="xs:text-xs sm:text-sm xs:text-center md:text-base lg:text-lg font-bodyFont tracking-wide leading-normal text-textDark">
+          Personalización de recomendaciones: Utilizamos la información sobre su
+          salud y bienestar para proporcionarle recomendaciones personalizadas
+          relacionadas con el uso de nuestros productos de Omega 3.
+        </li>
+        <li className="xs:text-xs sm:text-sm xs:text-center md:text-base lg:text-lg font-bodyFont tracking-wide leading-normal text-textDark">
+          Comunicaciones de marketing: Podemos utilizar su información personal
+          para enviarle comunicaciones de marketing sobre nuestros productos y
+          servicios relacionados con Nutrilite Omega 3, siempre y cuando usted
+          haya dado su consentimiento para recibir dichas comunicaciones.
+        </li>
+      </ul>
+      <h3 className="font-titleFont xs:text-base xs:text-center sm:text-base xs:pt-10 md:pt-0 md:text-lg lg:text-5xl font-semibold uppercase text-textDark text-center">
+        Protección de la información personal
+      </h3>
+      <p className="xs:text-xs sm:text-sm xs:text-center md:text-base lg:text-lg font-bodyFont tracking-wide leading-normal text-textDark">
+        En Nutrilite, nos comprometemos a proteger la seguridad de su
+        información personal. Hemos implementado medidas técnicas y
+        organizativas adecuadas para proteger su información contra el acceso no
+        autorizado, la divulgación, la alteración o la destrucción.
+      </p>{" "}
+      <h3 className="font-titleFont xs:text-base xs:text-center sm:text-base xs:pt-10 md:pt-0 md:text-lg lg:text-5xl font-semibold uppercase text-textDark text-center">
+        Compartir información personal
+      </h3>
+      <p className="xs:text-xs sm:text-sm xs:text-center md:text-base lg:text-lg font-bodyFont tracking-wide leading-normal text-textDark">
+        No vendemos ni alquilamos su información personal a terceros. Sin
+        embargo, podemos compartir su información personal con proveedores de
+        servicios de confianza que nos ayudan a realizar actividades
+        comerciales, como el procesamiento de pedidos y el envío de productos.
+        Estos proveedores de servicios están obligados a proteger su información
+        personal y solo pueden utilizarla para cumplir con sus responsabilidades
+        contractuales con nosotros.
+      </p>{" "}
+      <h3 className="font-titleFont xs:text-base xs:text-center sm:text-base xs:pt-10 md:pt-0 md:text-lg lg:text-5xl font-semibold uppercase text-textDark text-center">
+        Además, podemos compartir su información personal en los siguientes
+        casos:
+      </h3>
+      <p className="xs:text-xs sm:text-sm xs:text-center md:text-base lg:text-lg font-bodyFont tracking-wide leading-normal text-textDark">
+        {" "}
+        Cumplimiento legal: Podemos divulgar su información personal cuando sea
+        necesario para cumplir con la ley aplicable, los procesos legales o las
+        solicitudes gubernamentales.
+      </p>{" "}
+      <p className="xs:text-xs sm:text-sm xs:text-center md:text-base lg:text-lg font-bodyFont tracking-wide leading-normal text-textDark">
+        Protección de nuestros derechos: Podemos divulgar su información
+        personal cuando sea necesario para proteger nuestros derechos legales,
+        investigar fraudes, hacer cumplir los términos y condiciones aplicables
+        o responder a una emergencia que ponga en peligro la seguridad de
+        cualquier persona.
+      </p>
+      <h3 className="font-titleFont xs:text-base xs:text-center sm:text-base xs:pt-10 md:pt-0 md:text-lg lg:text-5xl font-semibold uppercase text-textDark text-center">
+        Cambios a la política de privacidad
+      </h3>
+      <p className="xs:text-xs sm:text-sm xs:text-center md:text-base lg:text-lg font-bodyFont tracking-wide leading-normal text-textDark">
+        Nos reservamos el derecho de actualizar o modificar esta política de
+        privacidad en cualquier momento y sin previo aviso. Le recomendamos que
+        revise periódicamente esta política de privacidad para estar informado
+        sobre cómo protegemos su información personal.
+      </p>{" "}
+      <h3 className="font-titleFont xs:text-base xs:text-center sm:text-base xs:pt-10 md:pt-0 md:text-lg lg:text-5xl font-semibold uppercase text-textDark text-center">
+        Contacto
+      </h3>
+      <p className="xs:text-xs sm:text-sm xs:text-center md:text-base lg:text-lg font-bodyFont tracking-wide leading-normal text-textDark">
+        Si tiene alguna pregunta, inquietud o solicitud relacionada con esta
+        política de privacidad o el uso de su información personal, no dude en
+        ponerse en contacto con nosotros a través de los canales de contacto
+        proporcionados en nuestro sitio web.
+      </p>{" "}
+      <p className="xs:text-xs sm:text-sm xs:text-center md:text-base lg:text-lg font-bodyFont tracking-wide leading-normal text-textDark">
+        Al utilizar los productos de Nutrilite Omega 3, usted reconoce que ha
+        leído, comprendido y aceptado esta política de privacidad. Agradecemos
+        su confianza en Nutrilite y nos comprometemos a proteger su privacidad y
+        garantizar el uso seguro de su información personal.
+      </p>
+    </section>
+  );
+};
+
+export default Terms;
