ed998c2a9c23885d749f0d4a141c8b6cba801a0f
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-06-09 14:34:21 -0300

    feat: Added and removed another things

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,29 +1,23 @@
 import mainPicture from "./images/product-picture-main.png";
-import garantia from "./images/garantia-cvl.png";
-import glister1 from "./images/prueba.jpg";
+import garantia from "./images/garantia-nutrilite.png";
 import testimo0 from "./images/testimonies/testimony-0.png";
 import testimo1 from "./images/testimonies/testimony-1.png";
-import testimo2 from "./images/testimonies/testimony-2.png";
-import testimo3 from "./images/testimonies/testimony-3.png";
-import testimo4 from "./images/testimonies/testimony-4.png";
-import testimo5 from "./images/testimonies/testimony-5.png";
-import comocepillo from "./images/como-un-cepillo.jpg";
-import ninapequena from "./images/nina-pequena.jpg";
-import superPreview from "./images/super-glister-preview.png";
-import chicaSonrisa from "./images/chica-sonrisa.jpg";
+import chicaConSonrisa from "./images/chica-con-sonrisa.jpg";
+import pescadoOmega from "./images/pescado-omega.jpg";
+import fitnessGirlOne from "./images/chica-fitness-one.jpg";
+import fitnessGirlTwo from "./images/chica-fitness-two.jpg";
+import coupleFitness from "./images/couple-fitness.jpg";
+import nutricionNutrilite from "./images/nutricion-nutrilite.jpeg";
 
 export {
   mainPicture,
   garantia,
-  glister1,
   testimo0,
   testimo1,
-  testimo2,
-  testimo3,
-  testimo4,
-  testimo5,
-  comocepillo,
-  ninapequena,
-  superPreview,
-  chicaSonrisa,
+  fitnessGirlTwo,
+  fitnessGirlOne,
+  chicaConSonrisa,
+  pescadoOmega,
+  coupleFitness,
+  nutricionNutrilite,
 };
