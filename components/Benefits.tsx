39801375cb1c766d64c17383033d1a9568daadfc
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-06-09 14:31:30 -0300

    feat: Implemented Benefits page

--- components/Benefits.tsx
+++ components/Benefits.tsx
@@ -1,24 +1,28 @@
-import { comocepillo, ninapequena } from "@/public/assets";
+import { coupleFitness, fitnessGirlTwo } from "@/public/assets";
 import Image from "next/image";
+import Link from "next/link";
 
 const Benefits = () => {
   return (
     <section className="w-full mx-auto py-10 mdl:py-24 flex flex-col items-center gap-4 lgl:gap-8 mdl:px-10 xl:px-4">
       <div className="w-10/12 my-10 flex flex-col items-center">
-        <h2 className="lg:text-4xl xs:text-sm sm:text-sm md:text-2xl font-bodyFont tracking-wide text-center text-textDark">
-          Otro detalle que hace que esta pasta de dientes sea diferente es el{" "}
-          <span className="underline">hecho de ser concentrada</span>. Solo se
-          necesita la cantidad de un{" "}
-          <span className="underline">pequeño guisante en el cepillo</span> de
-          dientes para un lavado completo.
+        <h2 className="lg:text-4xl xs:text-sm sm:text-sm md:text-2xl font-titleFont tracking-wide text-center text-textDark uppercase">
+          <span className="underline">Dos cápsulas diarias </span>te aportan los
+          ácidos grasos{" "}
+          <span className="underline">
+            fundamentales que tu cuerpo necesita.{" "}
+          </span>
+          Su fórmula balanceada contiene{" "}
+          <span className="underline">vitamina E y aporta 600mg EPA y DHA</span>
+          .
         </h2>
         <div className="flex drop-shadow-2x">
           <div className="w-1/2 flex py-5 flex-col items-center mr-2">
             <div>
               <Image
                 className="rounded-t-lg"
-                src={comocepillo}
-                alt="Una pequeña cantidad del tamaño de un guisante"
+                src={coupleFitness}
+                alt="Pareja haciendo ejercicio gracias al omega 3"
               />
             </div>
           </div>
@@ -26,32 +30,39 @@ const Benefits = () => {
             <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
               <Image
                 className="rounded-t-lg"
-                src={ninapequena}
-                alt="Chica pequeña cepillando sus dientes feliz"
+                src={fitnessGirlTwo}
+                alt="Mujer haciendo ejercicio gracias al omega 3"
               />
             </div>
           </div>
         </div>
-        <div className="w-full bg-white rounded-lg shadow-md shadow-btnColor1">
-          <h2 className="lg:text-4xl xs:text-sm sm:text-sm md:text-2xl font-bodyFont tracking-wide text-center text-black">
-            Sí, esa{" "}
-            <span className="font-bodyFont underline font-semibold text-btnColor1">
-              cantidad es suficiente para un lavado
+        <div className="w-full bg-white rounded-lg shadow-md shadow-colorBaseTwo">
+          <p className="lg:text-4xl xs:text-sm sm:text-sm md:text-2xl font-bodyFont tracking-wide text-center text-black py-5 px-5">
+            Al incorporar{" "}
+            <span className="font-bodyFont underline font-semibold text-colorshadow-colorBaseTwo">
+              Omega 3 Plus de Nutrilite en tu rutina diaria,
             </span>{" "}
-            con la espuma necesaria para distribuir el producto de manera
-            uniforme. Este beneficio también es posible gracias a la{" "}
-            <span className="font-bodyFont underline font-semibold text-btnColor1">
-              presencia de Xilitol
+            estarás proporcionando a tu cuerpo los nutrientes esenciales que a
+            menudo pueden ser escasos en la alimentación convencional.{" "}
+            <span className="font-bodyFont underline font-semibold text-colorshadow-colorBaseTwo">
+              Es especialmente recomendado para personas que no consumen
+              regularmente pescados{" "}
             </span>
-            , un ingrediente que aumenta ligeramente la salivación natural.
-          </h2>
+            ricos en omega-3, vegetarianos o personas que buscan un refuerzo
+            adicional para la salud del corazón y del cerebro.
+          </p>
         </div>
-        <h2 className="lg:text-4xl xs:text-sm sm:text-sm md:text-2xl font-bodyFont tracking-wide text-center mt-10">
-          <span className="underline">Tudo isso é apenas uma amostra</span> de
-          tudo o que você pode conquistar através desse mercado.
+        <h2 className="lg:text-4xl xs:text-sm sm:text-sm md:text-2xl font-bodyFont tracking-wide text-center text-textDark pt-10">
+          <span className="underline">Todo esto es solo el comienzo</span> de
+          todo lo que lograrás a través de este producto.
         </h2>
       </div>
-      <div className="w-1/5 border border-t border-gray-700"></div>
+      <Link href="https://wa.me/5524981579339" target="_blank">
+        <button className="w-full text-sm rounded-md text-textGreen tracking-wide bg-gradient-to-r from-colorBaseOne to-colorBaseTwo hover:from-colorBaseTwo hover:to-colorBaseOne font-bold py-2 xs:px-5 sm:px-5 md:px-8 lg:px-40 animate-bounce border shadow-md shadow-colorBaseTwo xs:h-20 uppercase font-titleSecondFont">
+          Quiero incorporar ya!
+        </button>
+      </Link>
+      <div className="w-1/5 border border-t border-colorBaseTwo"></div>
     </section>
   );
 };
